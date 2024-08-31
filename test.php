230d8a5c3769537ca42d14077c05c4c0a1505fb1
zubair shaikh <shaikhzuber@rapidinnovation.dev>
2024-08-31 14:10:55 +0530

    changed

--- test.php
+++ test.php
@@ -1,16 +1,71 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form Submission</title>
+</head>
+<body>
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-///retrive the data
-    $name = $_POST['name'];
-    $email =$_POST['email'];
-    $message =$_POST['message'];
+<?php
+$name = $email = $comment = "";
+$nameErr = $emailErr = $commentErr = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required!";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
 
-    echo $name;
-    echo $email;
-    echo $message;
-    echo "retrive";
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required!";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
 
+    if(empty(($_POST["comment"]))){
+        $comment = "";
+    }else{
+        $comment = test_input($_POST["comment"]);
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 ?>
+
+<h2>Simple Form</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Name: <input type="text" name="name" value="<?php echo $name;?>">
+    <span class="error">* <?php echo $nameErr;?></span>
+    <br><br>
+    
+    Email: <input type="email" name="email" value="<?php echo $email;?>">
+    <span class="error">* <?php echo $emailErr;?></span>
+    <br><br>
+
+    Email: <input type="text" name="comment" value="<?php echo $comment;?>">
+    <span class="error">* <?php echo $commentErr;?></span>
+    <br><br>
+    
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($nameErr) && empty($emailErr) && empty($commentErr)) {
+    echo "<h2>Your Input:</h2>";
+    echo "Name: " . $name;
+    echo "<br>";
+    echo "Email: " . $email;
+    echo "<br>";
+    echo "Comment: " . $comment;
+}
+?>
+
+</body>
+</html>
